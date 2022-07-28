f035fb4317ac0a6f5e06beb0dbd95ce3bac67192
Naail Kashif-Khan <naail28@gmail.com>
2022-07-28 11:35:59 +0100

    Fixed typo in `get_averaged_bhattacharyya_distances()` sanity print

--- esm/inverse_folding/interpretability_functions.py
+++ esm/inverse_folding/interpretability_functions.py
@@ -143,7 +143,7 @@ def get_averaged_bhattacharyya_distances(model, alphabet, sampled_sequence, devi
 
   for j in range(1, len(sampled_sequence)+1):
     if j % 10 == 0:
-      print(f"Residue {j} / {len(sampled_sequence+1)}")
+      print(f"Residue {j} / {len(sampled_sequence) + 1}")
     
     masked_coords, native_seq = extract_coords_from_structure(structure)
 
