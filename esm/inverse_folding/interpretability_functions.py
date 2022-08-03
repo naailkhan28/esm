5b8ffd5043d2120e717b6acebb215c0509f36b2d
Naail Kashif-Khan <102599656+naailkhan28@users.noreply.github.com>
2022-08-03 15:35:23 +0100

    Fixed quantizing padding mask sampling

--- esm/inverse_folding/interpretability_functions.py
+++ esm/inverse_folding/interpretability_functions.py
@@ -196,7 +196,7 @@ def get_averaged_bhattacharyya_distances_padding_mask(model, alphabet, sampled_s
   distances = {}
 
   percentage_values = torch.tensor(percentage_values)
-  masking_indices = int(percentage_values * len(sampled_sequence))
+  masking_indices = torch.round(percentage_values * len(sampled_sequence))
 
   for j in range(1, len(sampled_sequence)+1):
     if j % 10 == 0:
